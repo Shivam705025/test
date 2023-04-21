97d8174fa35ec91ac185e65bb6f79b85e5bc08d8
Shivam705025 <48374957+Shivam705025@users.noreply.github.com>
2023-04-22 01:28:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,40 +1,58 @@
- let model;
+// Create the scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
 
-// Load the model
-async function loadModel() {
-  model = await tf.loadGraphModel('model/model.json');
-  console.log('Model loaded');
-}
+// Add orbit controls to the camera
+const controls = new THREE.OrbitControls(camera, renderer.domElement);
+controls.target.set(0, 0, 0);
+controls.update();
 
-// Load an input image
-function loadImage(event) {
-  const image = document.getElementById('image');
-  image.src = URL.createObjectURL(event.target.files[0]);
-  image.onload = () => {
-    console.log('Image loaded');
+// Create the road
+const roadTexture = new THREE.TextureLoader().load('./textures/road.jpg');
+const roadMaterial = new THREE.MeshBasicMaterial({ map: roadTexture });
+const roadGeometry = new THREE.PlaneGeometry(20, 200);
+const road = new THREE.Mesh(roadGeometry, roadMaterial);
+road.rotation.x = -Math.PI / 2;
+scene.add(road);
+
+// Create the car
+const carGeometry = new THREE.BoxGeometry(2, 1, 4);
+const carMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const car = new THREE.Mesh(carGeometry, carMaterial);
+car.position.y = 0.5;
+scene.add(car);
+
+// Create the trees
+const treeGeometry = new THREE.BoxGeometry(1, 4, 1);
+const treeMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const tree1 = new THREE.Mesh(treeGeometry, treeMaterial);
+tree1.position.set(-5, 2, -50);
+const tree2 = new THREE.Mesh(treeGeometry, treeMaterial);
+tree2.position.set(5, 2, -50);
+scene.add(tree1);
+scene.add(tree2);
+
+// Create the buildings
+const buildingGeometry = new THREE.BoxGeometry(10, 20, 10);
+const buildingMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+const building1 = new THREE.Mesh(buildingGeometry, buildingMaterial);
+building1.position.set(-15, 10, -100);
+const building2 = new THREE.Mesh(buildingGeometry, buildingMaterial);
+building2.position.set(15, 10, -100);
+scene.add(building1);
+scene.add(building2);
+
+// Position the camera and start the game loop
+camera.position.set(0, 5, 10);
+const gameLoop = () => {
+  requestAnimationFrame(gameLoop);
+  car.position.z -= 0.1;
+  if (car.position.z < -100) {
+    car.position.z = 0;
   }
-}
-
-// Preprocess the input image
-function preprocessImage(image) {
-  const tensor = tf.browser.fromPixels(image)
-    .resizeNearestNeighbor([224, 224])
-    .toFloat()
-    .sub(127.5)
-    .div(127.5)
-    .expandDims();
-  return tensor;
-}
-
-// Classify the input image
-async function classify() {
-  const image = document.getElementById('image');
-  const tensor = preprocessImage(image);
-  const predictions = await model.predict(tensor).data();
-  const healthyProbability = predictions[0];
-  const unhealthyProbability = predictions[1];
-  const predictionText = `Healthy: ${Math.round(healthyProbability * 100)}%, Unhealthy: ${Math.round(unhealthyProbability * 100)}%`;
-  document.getElementById('prediction').textContent = predictionText;
-}
-
-// Load the model when the page is loaded
+  renderer.render(scene, camera);
+};
+gameLoop();
